182266c398b546bddd1c9181324d71a4c2a2dbde
Simon Annetts <simon@ateb.co.uk>
2013-08-15 16:55:33 +0100

    Needed to modify wiringPi.h to remove depricated references

--- build.sh
+++ build.sh
@@ -18,16 +18,29 @@ else
 	cd ..
 fi
 
+cat wiringPi/wiringPi/wiringPi.h |grep -v "wiringPiSetupPiFace" >wiringPi.h
+
 echo "Writing an interface file for SWIG...."
 echo "%module wiringPi" >$outfile
 echo "%{" >>$outfile
+echo "#include \"wiringPi.h\"" >>$outfile
 for i in wiringPi/wiringPi/*.o; do
+	j=`echo $i|rev|cut -c3-|rev`
+	[ -f $j.h ] && [ "$j" != "wiringPi/wiringPi/wiringPi" ] && echo "#include \"$j.h\"" >>$outfile
+done
+for i in wiringPi/devLib/*.o; do
 	j=`echo $i|rev|cut -c3-|rev`
 	[ -f $j.h ] && echo "#include \"$j.h\"" >>$outfile
 done
 echo "%}" >>$outfile
 echo >>$outfile
+
+echo "%include \"wiringPi.h\"" >>$outfile
 for i in wiringPi/wiringPi/*.o; do
+	j=`echo $i|rev|cut -c3-|rev`
+	[ -f $j.h ]  && [ "$j" != "wiringPi/wiringPi/wiringPi" ] && echo "%include \"$j.h\"" >>$outfile
+done
+for i in wiringPi/devLib/*.o; do
 	j=`echo $i|rev|cut -c3-|rev`
 	[ -f $j.h ] && echo "%include \"$j.h\"" >>$outfile
 done
@@ -37,10 +50,16 @@ swig -php $outfile
 
 echo "Compiling PHP module source...."
 gcc `php-config --includes` -fpic -c wiringPi_wrap.c
-gcc -shared wiringPi_wrap.o -o wiringPi.so
+#gcc -shared wiringPi_wrap.o -lwiringPi -o wiringPi.so
+gcc -shared wiringPi_wrap.o -lwiringPi -lwiringPiDev -o wiringPi.so
 
 echo "Copying wiringPi.so to PHP extensions dir..."
+echo "extension=`php-config --extension-dir`/wiringPi.so" >wiringPi.ini
+
 sudo cp -f wiringPi.so `php-config --extension-dir`/
+sudo cp -f wiringPi.ini /etc/php5/conf.d/90-wiringPi.ini
+sudo chown root:root `php-config --extension-dir`/wiringPi.so /etc/php5/conf.d/90-wiringPi.ini
+
 echo "There is a wiringPi.php include file that loads the module and provides a wiringPi class."
 echo "DONE!"
 
