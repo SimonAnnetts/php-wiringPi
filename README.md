16a1938b6c4d41e3eb42710146b59edcb7eb590f
Simon Annetts <simon@ateb.co.uk>
2021-02-19 17:41:43 +0000

    Update to work with PHP7.x on newer Raspian distro

--- README.md
+++ README.md
@@ -5,20 +5,18 @@ PHP bindings to the Raspberry PI wiringPi libraries.
 
 
 WARNING: This is an in-development library, it will not be bug free and fully featured.
-    Please email simon at ateb dot co dot uk if you have any problems, suggestions,
+    Please email jak at ateb dot co dot uk if you have any problems, suggestions,
     questions or words of support.
 
-WiringPi: An implementation of most of the Arduino Wiring
-        functions for the Raspberry Pi
-
+WiringPi: An implementation of most of the Arduino Wiring functions for the Raspberry Pi
 WiringPi2: WiringPi version 2 implements new functions for managing IO expanders.
 
 Testing:
-    Build with gcc version 4.6.3 (Debian 4.6.3-14+rpi1)
-    Built against PHP 5.4.4
+    Build with gcc (Raspbian 8.3.0-6+rpi1) 8.3.0
+    Built against PHP 7.3 (can be changed by setting the PHP version in the top of the build.sh script)
 
 Prerequisites:
-    You must have git, git-core, swig2.0, php5-dev, php5-cli and php5-common installed
+    You must have git, git-core, swig, php-dev, php-cli and php-common installed
     as well as the usual build tools gcc etc.
     
 
@@ -39,8 +37,8 @@ Look for the wiringPi module in the list and that there are no errors.
     
 The build script does the following things:
     
-	1) Downloads or updates the wiringPi library and utilities from https://git.drogon.net/wiringPi
+	1) Downloads or updates the wiringPi library and utilities from https://github.com/WiringPi/WiringPi.git
 	2) Builds and Installs the wiringPi library
 	3) Creates an interface file for SWIG using the wiringPi header files
 	4) Uses SWIG to create the PHP module source code and include files
-	5) Builds the PHP module source and then installs the shared module library (using sudo)
+	5) Builds the PHP module source and then installs the shared module library (using sudo) for php-cli and apache2 module
--- build.sh
+++ build.sh
@@ -1,64 +1,79 @@
 #!/bin/bash
 
+php_version=7.3
+swig_php_version=7
+
 outfile="wiringPi.i"
 
-if [ -d wiringPi ]; then
-	echo "Checking for and downloading newest version of wiringPi...."
-	cd wiringPi
+if [ -d WiringPi ]; then
+	echo "Checking for and downloading newest version of WiringPi...."
+	cd WiringPi
 	git pull
-	echo "Building and Installing wiringPi libraries and utilities..."
+	echo "Building and Installing WiringPi libraries and utilities..."
 	./build
 	cd ..
 else
-	echo "Downloading newest version of wiringPi...."
-	git clone git://git.drogon.net/wiringPi
-	cd wiringPi
-	echo "Building and Installing wiringPi libraries and utilities..."
+	echo "Downloading newest version of WiringPi...."
+	git clone https://github.com/WiringPi/WiringPi.git
+	cd WiringPi
+	echo "Building and Installing WiringPi libraries and utilities..."
 	./build
 	cd ..
 fi
 
-cat wiringPi/wiringPi/wiringPi.h |grep -v "wiringPiSetupPiFace" >wiringPi.h
+# cat wiringPi/wiringPi/wiringPi.h |grep -v "wiringPiSetupPiFace" >wiringPi.h
+cat WiringPi/wiringPi/wiringPi.h  >wiringPi.h
 
 echo "Writing an interface file for SWIG...."
-echo "%module wiringPi" >$outfile
-echo "%{" >>$outfile
-echo "#include \"wiringPi.h\"" >>$outfile
+echo "%module wiringPi" >${outfile}
+echo "%{" >>${outfile}
+echo "#include \"wiringPi.h\"" >>${outfile}
 for i in wiringPi/wiringPi/*.o; do
-	j=`echo $i|rev|cut -c3-|rev`
-	[ -f $j.h ] && [ "$j" != "wiringPi/wiringPi/wiringPi" ] && echo "#include \"$j.h\"" >>$outfile
+	j=$(echo $i|rev|cut -c3-|rev)
+	[ -f $j.h ] && [ "$j" != "wiringPi/wiringPi/wiringPi" ] && echo "#include \"$j.h\"" >>${outfile}
 done
 for i in wiringPi/devLib/*.o; do
-	j=`echo $i|rev|cut -c3-|rev`
-	[ -f $j.h ] && echo "#include \"$j.h\"" >>$outfile
+	j=$(echo $i|rev|cut -c3-|rev)
+	[ -f $j.h ] && echo "#include \"$j.h\"" >>${outfile}
 done
-echo "%}" >>$outfile
-echo >>$outfile
+echo "%}" >>${outfile}
+echo >>${outfile}
 
-echo "%include \"wiringPi.h\"" >>$outfile
+echo "%include \"wiringPi.h\"" >>${outfile}
 for i in wiringPi/wiringPi/*.o; do
-	j=`echo $i|rev|cut -c3-|rev`
-	[ -f $j.h ]  && [ "$j" != "wiringPi/wiringPi/wiringPi" ] && echo "%include \"$j.h\"" >>$outfile
+	j=$(echo $i|rev|cut -c3-|rev)
+	[ -f $j.h ]  && [ "$j" != "wiringPi/wiringPi/wiringPi" ] && echo "%include \"$j.h\"" >>${outfile}
 done
 for i in wiringPi/devLib/*.o; do
-	j=`echo $i|rev|cut -c3-|rev`
-	[ -f $j.h ] && echo "%include \"$j.h\"" >>$outfile
+	j=$(echo $i|rev|cut -c3-|rev)
+	[ -f $j.h ] && echo "%include \"$j.h\"" >>${outfile}
 done
 
 echo "Using SWIG to create PHP module source...."
-swig -php $outfile
+swig -php${swig_php_version} ${outfile}
+
+php_includes=$(php-config --includes)
+php_extensions=$(php-config --extension-dir)
 
 echo "Compiling PHP module source...."
-gcc `php-config --includes` -fpic -c wiringPi_wrap.c
-#gcc -shared wiringPi_wrap.o -lwiringPi -o wiringPi.so
+gcc ${php_includes} -fpic -c wiringPi_wrap.c
 gcc -shared wiringPi_wrap.o -lwiringPi -lwiringPiDev -o wiringPi.so
 
 echo "Copying wiringPi.so to PHP extensions dir..."
-echo "extension=`php-config --extension-dir`/wiringPi.so" >wiringPi.ini
+echo "extension=${php_extensions}/wiringPi.so" >wiringPi.ini
 
-sudo cp -f wiringPi.so `php-config --extension-dir`/
-sudo cp -f wiringPi.ini /etc/php5/conf.d/90-wiringPi.ini
-sudo chown root:root `php-config --extension-dir`/wiringPi.so /etc/php5/conf.d/90-wiringPi.ini
+sudo cp -f wiringPi.so ${php_extensions}/
+sudo chown root:root ${php_extensions}/wiringPi.so
+sudo chmod 644 ${php_extensions}/wiringPi.so
+
+sudo cp -f wiringPi.ini /etc/php/${php_version}/mods-available/wiringPi.ini
+sudo chown root:root /etc/php/${php_version}/mods-available/wiringPi.ini
+
+for i in cli apache2; do
+	if [ -d /etc/php/${php_version}/${i} ]; then
+		sudo ln -s /etc/php/${php_version}/mods-available/wiringPi.ini /etc/php/${php_version}/${i}/conf.d/20-wiringPi.ini
+	fi
+done
 
 echo "There is a wiringPi.php include file that loads the module and provides a wiringPi class."
 echo "DONE!"
